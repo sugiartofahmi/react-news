1b7720b3b6661232b39631be7ecd729ca4eabfb3
sugiartofahmi <sugiartofahmi@gmail.com>
2023-03-05 15:33:44 +0700

    Fahmi Sugiarto | Complete Project

--- src/layouts/GridLayout/index.jsx
+++ src/layouts/GridLayout/index.jsx
@@ -1,6 +1,6 @@
 const GridLayout = ({ children }) => {
   return (
-    <div className="grid md:grid-cols-3 grid-cols-2 md:gap-5 gap-2 ">
+    <div className="grid md:grid-cols-3 grid-cols-2 md:gap-5 gap-2 p-5 ">
       {children}
     </div>
   );
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,41 +1,35 @@
-import { lazy } from "react";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
+import { Fragment, Suspense, lazy } from "react";
+
 import { useRecoilValue } from "recoil";
 import { fetchNews, paramState } from "../../store";
+import { ErrorBoundary } from "react-error-boundary";
 const GridLayout = lazy(() => import("../../layouts/GridLayout"));
-const MainLayout = lazy(() => import("../../layouts/MainLayout"));
-const ContentLayout = lazy(() => import("../../layouts/ContentLayout"));
-const Loading = lazy(() => import("../../components/Loading"));
+const Navbar = lazy(() => import("../../components/Navbar"));
+
 const Card = lazy(() => import("../../components/Card"));
 const Home = () => {
   const news = useRecoilValue(fetchNews);
 
   return (
-    <Suspense fallback={<Loading />}>
-      <MainLayout>
-        <ErrorBoundary
-          fallback={<h1 className="text-red-500">Could not fetch data.</h1>}
-        >
-          <Suspense fallback={<Loading />}>
-            <ContentLayout>
-              <GridLayout>
-                {news.length > 0 &&
-                  news.map((el, i) => (
-                    <Card
-                      url={el.url}
-                      title={el.title}
-                      description={el.description}
-                      image={el.urlToImage}
-                      key={i}
-                    />
-                  ))}
-              </GridLayout>
-            </ContentLayout>
-          </Suspense>
-        </ErrorBoundary>
-      </MainLayout>
-    </Suspense>
+    <Fragment>
+      <Navbar />
+      <ErrorBoundary>
+        <Suspense>
+          <GridLayout>
+            {news.length > 0 &&
+              news.map((el, i) => (
+                <Card
+                  url={el.url}
+                  title={el.title}
+                  description={el.description}
+                  image={el.urlToImage}
+                  key={i}
+                />
+              ))}
+          </GridLayout>
+        </Suspense>
+      </ErrorBoundary>
+    </Fragment>
   );
 };
 
